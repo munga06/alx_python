9487af3d11df722a0208cbe49ac7791c28c24918
munga06 <munga21407@gmail.com>
2023-08-02 20:54:43 +0300

    mym

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,4 +4,17 @@
 
 class Square:
     """ Represent a class square """
-    pass
+    def __init__(self, size):
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def perimeter(self):
+        return 4 * self.__size
+
+    def get_size(self):
+        return self.__size
+
+    def set_size(self, size):
+        self.__size = size
--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,12 +3,13 @@
 
 
 class Square:
-    """ Description of square """
-    def __init__(self, size=0):
-        """ initialize size"""
-
+    class Square:
+     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
