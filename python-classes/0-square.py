028debf7c03856d6409475a53c30dae12d52ebae
munga06 <munga21407@gmail.com>
2023-09-15 09:48:25 +0300

    try

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ Define a class Square """
 class Square:
+    """ Represents a square"""
     def __init__(self, size):
         self.__size = size
     self.__size = size
     def area(self):
         return self.__size * self.__size
-
-    def __str__(self):
+""" Define a class Square """
+def __str__(self):
         return f"Square with side length {self.__size}"
 
